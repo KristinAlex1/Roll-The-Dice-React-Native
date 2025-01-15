dc0145d5531d2889e6b38d056430b86228a995f9
KristinAlex1 <kristinalex2001@gmail.com>
2025-01-15 14:14:36 -0400

    first commit

--- App.tsx
+++ App.tsx
@@ -1,118 +1,106 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
+  TouchableOpacity,
   View,
 } from 'react-native';
+import React, {useState} from 'react';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+export default function App() {
+  const [diceValue, setDiceValue] = useState(1);
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  const diceRoll = () => {
+    let roll = 1;
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    for (let i = 1; i < 7; i++) {
+      roll = Math.floor(Math.random() * 7);
+    }
+    {
+      roll === 0 ? diceRoll() : setDiceValue(roll);
+    }
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.appContainer}>
+        <View style={styles.dice}>
+          <View style={styles.diceRow1}>
+            <View style={styles.diceValue}></View>
+            <View style={styles.diceValue}></View>
+            <View style={styles.diceValue}></View>
+            <View style={styles.diceValue}></View>
+            <View style={styles.diceValue}></View>
+            <View style={styles.diceValue}></View>
+            <View style={styles.diceValue}></View>
+            <View style={styles.diceValue}></View>
+            <View style={styles.diceValue}></View>
+          </View>
         </View>
-      </ScrollView>
+        
+        <Text style={styles.diceTxt}>{diceValue}</Text>
+
+        <TouchableOpacity onPress={diceRoll} style={styles.Button}>
+          <Text style={styles.rollTxt}>Roll</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  safeArea: {
+    flex: 1,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+
+  appContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+
+  dice: {
+    height: 300,
+    width: 300,
+    borderRadius: 30,
+    backgroundColor: 'lightblue',
+    flexDirection: 'row',
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexWrap: 'wrap'
   },
-  highlight: {
-    fontWeight: '700',
+  diceTxt: {
+    fontSize: 20,
+  },
+  diceRow1:{
+    flexDirection: 'row',
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexWrap: 'wrap'
+
+  },
+  Button: {
+    marginVertical: 20,
+    height: 50,
+    width: 100,
+    borderRadius: 20,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  rollTxt: {
+    fontSize: 20,
+  },
+  diceValue: {
+    height: 60,
+    width: 60,
+    backgroundColor: 'blue',
+    borderRadius: 30,
+    marginHorizontal: 12,
+    marginVertical: 18,
+    marginBottom: 20
+    
   },
 });
-
-export default App;
