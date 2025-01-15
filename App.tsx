aebaaf37b1ac68168bafe3efb99e3606b3ef375e
KristinAlex1 <kristinalex2001@gmail.com>
2025-01-15 19:27:11 -0400

    Added Logic

--- App.tsx
+++ App.tsx
@@ -9,6 +9,67 @@ import React, {useState} from 'react';
 
 export default function App() {
   const [diceValue, setDiceValue] = useState(1);
+  const [diceColor1, setDiceColor1] = useState('lightblue');
+  const [diceColor2, setDiceColor2] = useState('lightblue');
+  const [diceColor3, setDiceColor3] = useState('lightblue');
+  const [diceColor4, setDiceColor4] = useState('lightblue');
+  const [diceColor5, setDiceColor5] = useState('lightblue');
+  const [diceColor6, setDiceColor6] = useState('lightblue');
+  const [diceColor7, setDiceColor7] = useState('lightblue');
+  const [diceColor8, setDiceColor8] = useState('lightblue');
+  const [diceColor9, setDiceColor9] = useState('lightblue');
+
+
+  function setValue(roll) {
+
+    if (roll === 1){
+      setDiceColor5('black');
+    }
+    if (roll === 2){
+      setDiceColor1('black');
+      setDiceColor9('black');
+    }
+    if (roll === 3){
+      setDiceColor1('black');
+      setDiceColor5('black');
+      setDiceColor9('black');
+    }
+    if (roll === 4){
+      setDiceColor1('black');
+      setDiceColor3('black');
+      setDiceColor7('black');
+      setDiceColor9('black');
+    }
+    if (roll === 5){
+      setDiceColor1('black');
+      setDiceColor3('black');
+      setDiceColor5('black');
+      setDiceColor7('black');
+      setDiceColor9('black');
+    }
+    if (roll === 6){
+      setDiceColor1('black');
+      setDiceColor4('black');
+      setDiceColor4('black');
+      setDiceColor6('black');
+      setDiceColor7('black');
+      setDiceColor9('black');
+    }
+
+  } 
+
+  function resetDice() {
+    setDiceColor1('lightblue');
+    setDiceColor2('lightblue');
+    setDiceColor3('lightblue');
+      setDiceColor4('lightblue');
+      setDiceColor5('lightblue');
+      setDiceColor6('lightblue');
+      setDiceColor7('lightblue');
+      setDiceColor9('lightblue');
+      setDiceColor8('lightblue');
+
+  }
 
   const diceRoll = () => {
     let roll = 1;
@@ -16,9 +77,11 @@ export default function App() {
     for (let i = 1; i < 7; i++) {
       roll = Math.floor(Math.random() * 7);
     }
-    {
-      roll === 0 ? diceRoll() : setDiceValue(roll);
-    }
+    
+    {roll === 0 ? diceRoll() : setDiceValue(roll)};
+    resetDice();
+    setValue(roll);
+    
   };
 
   return (
@@ -26,19 +89,19 @@ export default function App() {
       <View style={styles.appContainer}>
         <View style={styles.dice}>
           <View style={styles.diceRow1}>
-            <View style={styles.diceValue}></View>
-            <View style={styles.diceValue}></View>
-            <View style={styles.diceValue}></View>
-            <View style={styles.diceValue}></View>
-            <View style={styles.diceValue}></View>
-            <View style={styles.diceValue}></View>
-            <View style={styles.diceValue}></View>
-            <View style={styles.diceValue}></View>
-            <View style={styles.diceValue}></View>
+            <View style={[{ backgroundColor: diceColor1 },styles.diceValue ]}/>
+            <View style={[{ backgroundColor: diceColor2  },styles.diceValue ]}/>
+            <View style={[{ backgroundColor: diceColor3  },styles.diceValue ]}/>
+            <View style={[{ backgroundColor: diceColor4  },styles.diceValue ]}/>
+            <View style={[{ backgroundColor: diceColor5  },styles.diceValue ]}/>
+            <View style={[{ backgroundColor: diceColor6  },styles.diceValue ]}/>
+            <View style={[{ backgroundColor: diceColor7  },styles.diceValue ]}/>
+            <View style={[{ backgroundColor: diceColor8  },styles.diceValue ]}/>
+            <View style={[{ backgroundColor: diceColor9  },styles.diceValue ]}/>
           </View>
         </View>
         
-        <Text style={styles.diceTxt}>{diceValue}</Text>
+        
 
         <TouchableOpacity onPress={diceRoll} style={styles.Button}>
           <Text style={styles.rollTxt}>Roll</Text>
@@ -75,7 +138,7 @@ const styles = StyleSheet.create({
   },
   diceRow1:{
     flexDirection: 'row',
-    alignSelf: 'center',
+    
     justifyContent: 'center',
     alignItems: 'center',
     flexWrap: 'wrap'
@@ -96,11 +159,10 @@ const styles = StyleSheet.create({
   diceValue: {
     height: 60,
     width: 60,
-    backgroundColor: 'blue',
     borderRadius: 30,
-    marginHorizontal: 12,
-    marginVertical: 18,
-    marginBottom: 20
+    margin:17,
+    
+    
     
   },
 });
